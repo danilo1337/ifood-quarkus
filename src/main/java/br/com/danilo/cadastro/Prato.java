d29afb1a76b1779ef93de9088f7c126c1d9fe090
danilo1337 <danilorrg@gmail.com>
2023-11-13 19:10:54 -0300

    Em desenvolvimento

---
+++ src/main/java/br/com/danilo/cadastro/Prato.java
@@ -0,0 +1,28 @@
+package br.com.danilo.cadastro;
+
+import java.math.BigDecimal;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "prato")
+public class Prato extends PanacheEntityBase {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+
+    public String descricaco;
+
+    @ManyToOne
+    public Restaurante restaurante;
+
+    public BigDecimal preco;
+
+}
---
+++ src/main/java/br/com/danilo/cadastro/RestauranteResource.java
@@ -0,0 +1,65 @@
+package br.com.danilo.cadastro;
+
+import java.util.List;
+import java.util.Optional;
+
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.NotFoundException;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.Response.Status;
+
+@Path("/restaurantes")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class RestauranteResource {
+
+    @GET
+    public List<Restaurante> buscar() {
+        return Restaurante.listAll();
+    }
+
+    @POST
+    @Transactional
+    public Response adiicionar(Restaurante restaurante) {
+        restaurante.persist();
+        return Response.status(Status.CREATED).build();
+    }
+
+    @PUT
+    @Path("{id}")
+    @Transactional
+    public void atualizar(@PathParam("id") Long id, Restaurante dto) {
+        Optional<Restaurante> restauranteOp = Restaurante.findByIdOptional(id);
+
+        if (restauranteOp.isEmpty()) {
+            throw new NotFoundException();
+        }
+
+        Restaurante restaurante = restauranteOp.get();
+        restaurante.nome = dto.nome;
+
+        restaurante.persist();
+    }
+
+    @DELETE
+    @Path("{id}")
+    @Transactional
+    public void deletar(@PathParam("id") Long id) {
+        Optional<Restaurante> restauranteOp = Restaurante.findByIdOptional(id);
+
+        restauranteOp.ifPresentOrElse(Restaurante::delete, () -> {
+            throw new NotFoundException();
+        });
+
+    }
+
+}
